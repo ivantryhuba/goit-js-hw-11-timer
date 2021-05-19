dc193b5d8c3c6c280bce18717ed6e5d7dc2e0488
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-05-19 22:28:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,36 +12,43 @@ function diffSubtract(date1, date2) {
   return date2 - date1;
 }
 
-// JSON в котором храним дату назначения
+// Объект в котором храним дату назначения
 let endDate = {
-  fullYear: "2022",
+  fullYear: "2021",
   month: "05",
-  day: "18",
-  hours: "17",
-  minutes: "09",
+  day: "20",
+  hours: "00",
+  minutes: "00",
   seconds: "00",
 };
 
-// Строка вивода времени
-let endDateString = `${endDate.fullYear}-${endDate.month}-${endDate.day}T${endDate.hours}:${endDate.minutes}:${endDate.seconds}`;
+// Строка вывода времени
+let endDateString = `${endDate.fullYear}-${endDate.month}-${endDate.day} ${endDate.hours}:${endDate.minutes}:${endDate.seconds}`;
+console.log('endDateString'+endDateString);
 
 // Запускаем таймер с интервалом в одну секунду
 const timer = setInterval(() => {
   // Получаем нынешнее время
   let nowDate = new Date();
+  console.log("nowDate" + nowDate);
+
   // Получаем заданное время
   let targetDate = new Date(endDateString);
+  console.log("targetDate" + targetDate);
+
   // Вычесляем время которое осталось
   let remainingTime = diffSubtract(nowDate, targetDate);
+  console.log("remainingTime" + remainingTime);
 
   if (remainingTime <= 0) {
-      clearInterval(timer);
-      console.log('Время вышло')
+    clearInterval(timer);
+    console.log("Время вышло");
   } else {
     let newTime = new Date(remainingTime);
-    refs.daysEl.textContent = `${newTime.getUTCDate()}`;
+    refs.daysEl.textContent = `${newTime.getUTCDate()-1}`;
     refs.hoursEl.textContent = `${newTime.getUTCHours()}`;
     refs.minsEl.textContent = `${newTime.getUTCMinutes()}`;
     refs.secsEl.textContent = `${newTime.getUTCSeconds()}`;
+    console.log(newTime)
   }
 }, 1000);
