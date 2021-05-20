aa4805befd4abb964c5a67a3a06299231a29f3e5
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-05-21 00:28:20 +0300

    Переписывает таймер

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// Элементы таймера
+'use strict'
+// !Элементы таймера
 const refs = {
   timerEl: document.querySelector("#timer-1"),
   daysEl: document.querySelector('[data-value="days"]'),
@@ -7,48 +8,64 @@ const refs = {
   secsEl: document.querySelector('[data-value="secs"]'),
 };
 
-// Функция которая считает разницу времени
-function diffSubtract(date1, date2) {
-  return date2 - date1;
+// !Класс таймера
+class CountdownTimer {
+  constructor({selector, targetDate}) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  start = setInterval(() => {
+    const nowDate = Date.now();
+    const remainingTime = this.targetDate - nowDate;
+    const time = this.getTimeComponents(remainingTime);
+
+    this.updateClockface(time);
+    this.stop(time)
+  }, 1000);
+
+  stop(time) {
+    if (time <= 0) {
+      clearInterval(this.start)
+      refs.timerEl.textContent = 'Your time is over-GOOD BYE=))'
+    }
+  }
+
+  // !Считаем сколько чего влазит в количество милисекунд
+  getTimeComponents(time) {
+    //  TODO Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+    //  TODO Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора остатка % и делим его на количество миллисекунд в одном часе (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+
+    // TODO Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+    // TODO Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество миллисекунд в одной секунде (1000)
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  // !Принимаем число, приводим к строке и добавляем в начало 0, если число меньше 2-х символов
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  // !Обновляем данные в разметке
+  updateClockface({ days, hours, mins, secs }) {
+    refs.daysEl.textContent = `${days}`;
+    refs.hoursEl.textContent = `${hours}`;
+    refs.minsEl.textContent = `${mins}`;
+    refs.secsEl.textContent = `${secs}`;
+  }
 }
 
-// Объект в котором храним дату назначения
-let endDate = {
-  fullYear: "2021",
-  month: "05",
-  day: "20",
-  hours: "00",
-  minutes: "00",
-  seconds: "00",
-};
-
-// Строка вывода времени
-let endDateString = `${endDate.fullYear}-${endDate.month}-${endDate.day} ${endDate.hours}:${endDate.minutes}:${endDate.seconds}`;
-console.log('endDateString'+endDateString);
-
-// Запускаем таймер с интервалом в одну секунду
-const timer = setInterval(() => {
-  // Получаем нынешнее время
-  let nowDate = new Date();
-  console.log("nowDate" + nowDate);
-
-  // Получаем заданное время
-  let targetDate = new Date(endDateString);
-  console.log("targetDate" + targetDate);
-
-  // Вычесляем время которое осталось
-  let remainingTime = diffSubtract(nowDate, targetDate);
-  console.log("remainingTime" + remainingTime);
-
-  if (remainingTime <= 0) {
-    clearInterval(timer);
-    console.log("Время вышло");
-  } else {
-    let newTime = new Date(remainingTime);
-    refs.daysEl.textContent = `${newTime.getUTCDate()-1}`;
-    refs.hoursEl.textContent = `${newTime.getUTCHours()}`;
-    refs.minsEl.textContent = `${newTime.getUTCMinutes()}`;
-    refs.secsEl.textContent = `${newTime.getUTCSeconds()}`;
-    console.log(newTime)
-  }
-}, 1000);
+// !Создаём новый таймер
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jan 01, 2022'),
+});
